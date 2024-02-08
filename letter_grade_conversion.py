4f6ee6a67a51f7162cafe2604d4c3d8f2fb11a72
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-08 09:29:28 -0700

    add school class, calc letter grade method, main

--- letter_grade_conversion.py
+++ letter_grade_conversion.py
@@ -6,3 +6,31 @@ class LetterGrade(Enum):
     C = auto()
     D = auto()
     F = auto()
+
+class School:
+    def __init__(self):
+        pass
+
+    def calculate_letter_grade(grade_percentage: int) -> LetterGrade:
+        match grade_percentage:
+            case grade_percentage >= 90:
+                return LetterGrade.A
+            case grade_percentage >= 80:
+                return LetterGrade.B
+            case grade_percentage >= 70:
+                return LetterGrade.C
+            case grade_percentage >= 60:
+                return LetterGrade.D
+            case grade_percentage < 60:
+                return LetterGrade.F
+            case _:
+                return "Invalid grade percentage"
+
+def main():
+    school = School()
+    grade = school.calculate_letter_grade(78)
+    print(grade)
+    print(grade.value)
+
+if __name__ == "__main__":
+    main()
